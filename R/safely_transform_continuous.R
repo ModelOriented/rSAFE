51780c24ac7d3f3c46c7c050d1f3c3cc73c1b3c3
AnnaGierlak <gierlaka@student.mini.pw.edu.pl>
2019-04-28 00:58:28 +0200

    segmentation based on median in case of no breakpoints

--- R/safely_transform_continuous.R
+++ R/safely_transform_continuous.R
@@ -29,17 +29,34 @@ safely_transform_continuous <- function(variable, explainer, package = "changepo
   }
 
   if (length(break_points) == 0) { #no significant changes have been found
-    break_points <- NULL
-    new_levels <- NULL
+    # break_points <- NULL
+    # new_levels <- NULL
+    #in this case we take a median as a breakpoint and create two intervals
+    break_points <- median(explainer$data[[variable]])
+    #we use prettyNum() function to nicely format numbers that will be displeyed during intervals printing
+    new_levels <- c(paste0("(-Inf, ", prettyNum(break_points[1]), ")"),
+                    paste0("[", prettyNum(break_points[1]), ", Inf)"))
+
   } else {
     break_points <- sv$x[break_points]
     if (length(break_points) == 1) {
-      new_levels <- c(paste0("(-Inf, ", break_points[1], ")"),
-                      paste0("[", break_points[1], ", Inf)"))
+      #we use prettyNum() function to nicely format numbers that will be displeyed during intervals printing
+      new_levels <- c(paste0("(-Inf, ", prettyNum(break_points[1]), ")"),
+                      paste0("[", prettyNum(break_points[1]), ", Inf)"))
     } else {
-      new_levels <- c(paste0("(-Inf, ", break_points[1], ")"),
-                      apply(matrix(c(break_points[1:(length(break_points)-1)],break_points[2:length(break_points)]), ncol = 2), 1, function(x) paste0("[", x[1], ", ", x[2], ")")),
-                      paste0("[", break_points[length(break_points)], ", Inf)"))
+      #in case there are many breakpoints first we check if they still will be unique after applying prettyNum() function
+      bp_pretty <- unique(prettyNum(break_points))
+      if (length(bp_pretty) == length(break_points)) { #all 'pretty' breakpoints are unique so they can be used to create intervals
+        new_levels <- c(paste0("(-Inf, ", prettyNum(break_points[1]), ")"),
+                        apply(matrix(c(prettyNum(break_points[1:(length(break_points)-1)]),prettyNum(break_points[2:length(break_points)])), ncol = 2), 1, function(x) paste0("[", x[1], ", ", x[2], ")")),
+                        paste0("[", prettyNum(break_points[length(break_points)]), ", Inf)"))
+      } else { #not all the 'pretty' breakpoints are unique so we leave original formatting
+        new_levels <- c(paste0("(-Inf, ", break_points[1], ")"),
+                        apply(matrix(c(break_points[1:(length(break_points)-1)],break_points[2:length(break_points)]), ncol = 2), 1, function(x) paste0("[", x[1], ", ", x[2], ")")),
+                        paste0("[", break_points[length(break_points)], ", Inf)"))
+      }
+
+
     }
   }
 
